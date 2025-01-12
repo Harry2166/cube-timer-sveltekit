52c1e7f6b2066bda2fe13d39447358ed3c7372d4
Harry2166 <harry2166.hq@gmail.com>
2025-01-12 19:41:02 +0800

    feat: polish the UI of the login screen

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { ActionData } from './$types';
-
+	import { Button, Label, Input, Helper } from 'flowbite-svelte';
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
 <form method="post" action="?/login" use:enhance>
-	<label>
-		Username
-		<input name="username" />
-	</label>
-	<label>
-		Password
-		<input type="password" name="password" />
-	</label>
-	<button>Login</button>
-	<button formaction="?/register">Register</button>
+	<div class="mb-6">
+		<Label for="username" class="block mb-2">Username</Label>
+		<Input name="username" id="username" placeholder="Type Your Username"/>
+	</div>
+	<div class="mb-6">
+		<Label for="password" class="block mb-2">Password</Label>
+		<Input name="password" type="password" id="password" placeholder="Type Your Password"/>
+		<Helper class='mt-2' color='red'><span class="font-medium">{form?.message ?? ''}</span></Helper>
+	</div>
+	<Button type="submit">Login</Button>
+	<Button type="submit" formaction="?/register">Register</Button>
 </form>
-<p style="color: red">{form?.message ?? ''}</p>
