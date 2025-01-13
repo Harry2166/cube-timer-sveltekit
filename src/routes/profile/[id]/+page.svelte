c23db3e1bccd823572dda72da26630a467e35743
Harry2166 <harry2166.hq@gmail.com>
2025-01-13 12:51:53 +0800

    refactor: combine the profile page into an entire div

--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -123,84 +123,82 @@
 
 </script>
 
-<Navbar username={data.navbar_stuff[0].username} user_id={data.navbar_stuff[0].id} scramble={""}></Navbar>
-<br>
-<div class="flex items-center justify-center gap-4">
-	<Button>Pick Event<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
-    <Dropdown>
-        <DropdownItem onclick={() => changeEvent("333")}>3x3</DropdownItem>
-        <DropdownItem onclick={() => changeEvent("444")}>4x4</DropdownItem>
-    </Dropdown>
-</div>
-<br>
-<Table hoverable={true}>
-    <TableHead>
-    <TableHeadCell>Time</TableHeadCell>
-    <TableHeadCell>Scramble</TableHeadCell>
-    <TableHeadCell>Recorded At:</TableHeadCell>
-    <TableHeadCell>DNF</TableHeadCell>
-    <TableHeadCell>+2</TableHeadCell>
-    <TableHeadCell>
-        <span class="sr-only"></span>
-    </TableHeadCell>
-    </TableHead>
-    <TableBody tableBodyClass="divide-y">
-        {#each shownSolves as solve}
-        <TableBodyRow>
-            <TableBodyCell>{showTime(solve)}</TableBodyCell>
-            <TableBodyCell>{solve.scramble}</TableBodyCell>
-            <TableBodyCell>{new Date(solve.timeRecord)}</TableBodyCell>
-            <TableBodyCell>
-                {#if !solve.isDNF}
-                    <Button onclick={async () => {updateTime(solve.solveId, 1, solve.isPlusTwo)}}>DNF</Button>
-                {:else}
-                    <Button color="blue" onclick={async () => {updateTime(solve.solveId, 0, solve.isPlusTwo)}}>DNF</Button>
-                {/if}
-            </TableBodyCell>
-            <TableBodyCell>
-                {#if !solve.isPlusTwo}
-                    <Button onclick={async () => {updateTime(solve.solveId, solve.isDNF, 1)}}>+2</Button>
-                {:else}
-                    <Button color="blue" onclick={async () => {updateTime(solve.solveId, solve.isDNF, 0)}}>+2</Button>
-                {/if}
-            </TableBodyCell>
-            <TableBodyCell>
-                <Button onclick={async () => {deleteTime(solve.solveId)}}>Delete</Button>
-            </TableBodyCell>
-        </TableBodyRow>
-        {/each}
-        {#if shownSolves.length != 5}
-            {#each {length: 5 - shownSolves.length} as _,i}
+<div class="flex flex-col gap-4">
+    <Navbar username={data.navbar_stuff[0].username} user_id={data.navbar_stuff[0].id} scramble={""}></Navbar>
+    <div class="flex items-center justify-center gap-4">
+        <Button>Pick Event<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
+        <Dropdown>
+            <DropdownItem onclick={() => changeEvent("333")}>3x3</DropdownItem>
+            <DropdownItem onclick={() => changeEvent("444")}>4x4</DropdownItem>
+        </Dropdown>
+    </div>
+    <Table hoverable={true}>
+        <TableHead>
+        <TableHeadCell>Time</TableHeadCell>
+        <TableHeadCell>Scramble</TableHeadCell>
+        <TableHeadCell>Recorded At:</TableHeadCell>
+        <TableHeadCell>DNF</TableHeadCell>
+        <TableHeadCell>+2</TableHeadCell>
+        <TableHeadCell>
+            <span class="sr-only"></span>
+        </TableHeadCell>
+        </TableHead>
+        <TableBody tableBodyClass="divide-y">
+            {#each shownSolves as solve}
             <TableBodyRow>
-                <TableBodyCell></TableBodyCell>
-                <TableBodyCell></TableBodyCell>
-                <TableBodyCell></TableBodyCell>
-                <TableBodyCell></TableBodyCell>
-                <TableBodyCell></TableBodyCell>
-                <!-- <TableBodyCell>{solve.event}</TableBodyCell> -->
+                <TableBodyCell>{showTime(solve)}</TableBodyCell>
+                <TableBodyCell>{solve.scramble}</TableBodyCell>
+                <TableBodyCell>{new Date(solve.timeRecord)}</TableBodyCell>
                 <TableBodyCell>
-                    <Button class="opacity-0">Delete</Button>
+                    {#if !solve.isDNF}
+                        <Button onclick={async () => {updateTime(solve.solveId, 1, solve.isPlusTwo)}}>DNF</Button>
+                    {:else}
+                        <Button color="blue" onclick={async () => {updateTime(solve.solveId, 0, solve.isPlusTwo)}}>DNF</Button>
+                    {/if}
+                </TableBodyCell>
+                <TableBodyCell>
+                    {#if !solve.isPlusTwo}
+                        <Button onclick={async () => {updateTime(solve.solveId, solve.isDNF, 1)}}>+2</Button>
+                    {:else}
+                        <Button color="blue" onclick={async () => {updateTime(solve.solveId, solve.isDNF, 0)}}>+2</Button>
+                    {/if}
+                </TableBodyCell>
+                <TableBodyCell>
+                    <Button onclick={async () => {deleteTime(solve.solveId)}}>Delete</Button>
                 </TableBodyCell>
             </TableBodyRow>
             {/each}
-        {/if}
-    </TableBody>
-</Table>
-<br>
-<div class="flex flex-col items-center justify-center gap-2">
-    <div class="text-sm text-gray-700 dark:text-gray-400">
-        Showing <span class="font-semibold text-gray-900 dark:text-white">{rangeOfShownSolves + 1}</span>
-        to
-        <span class="font-semibold text-gray-900 dark:text-white">{maxOfGivenRange}</span>
-        of
-        <span class="font-semibold text-gray-900 dark:text-white">{eventOnlySolves.length}</span>
-        Entries
-    </div>
+            {#if shownSolves.length != 5}
+                {#each {length: 5 - shownSolves.length} as _,i}
+                <TableBodyRow>
+                    <TableBodyCell></TableBodyCell>
+                    <TableBodyCell></TableBodyCell>
+                    <TableBodyCell></TableBodyCell>
+                    <TableBodyCell></TableBodyCell>
+                    <TableBodyCell></TableBodyCell>
+                    <!-- <TableBodyCell>{solve.event}</TableBodyCell> -->
+                    <TableBodyCell>
+                        <Button class="opacity-0">Delete</Button>
+                    </TableBodyCell>
+                </TableBodyRow>
+                {/each}
+            {/if}
+        </TableBody>
+    </Table>
+    <div class="flex flex-col items-center justify-center gap-2">
+        <div class="text-sm text-gray-700 dark:text-gray-400">
+            Showing <span class="font-semibold text-gray-900 dark:text-white">{rangeOfShownSolves + 1}</span>
+            to
+            <span class="font-semibold text-gray-900 dark:text-white">{maxOfGivenRange}</span>
+            of
+            <span class="font-semibold text-gray-900 dark:text-white">{eventOnlySolves.length}</span>
+            Entries
+        </div>
 
-    <div class="flex space-x-3 rtl:space-x-reverse">
-    <PaginationItem on:click={decreaseRange}>Previous</PaginationItem>
-    <PaginationItem on:click={increaseRange}>Next</PaginationItem>
+        <div class="flex space-x-3 rtl:space-x-reverse">
+        <PaginationItem on:click={decreaseRange}>Previous</PaginationItem>
+        <PaginationItem on:click={increaseRange}>Next</PaginationItem>
+        </div>
     </div>
+    <Footer></Footer>
 </div>
-
-<Footer></Footer>
