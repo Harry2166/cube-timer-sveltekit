8455c26ecd1c6f829a905dc82056db183965290e
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-01-14 22:42:24 +0800

    feat: make the timer size based on screen size of the user

--- src/routes/Timer.svelte
+++ src/routes/Timer.svelte
@@ -5,17 +5,17 @@
 <div class="flex items-center justify-center">
     {#if !timerStart && !spacebarPressed}
         {#if time.value.minutes == 0}
-            <p class="text-[256px] dark:text-white text-center">{time.value.seconds}.{time.value.miliseconds}</p>
+            <p class="dark:text-white text-center" style="font-size: clamp(2rem, 10vw, 16rem)">{time.value.seconds}.{time.value.miliseconds}</p>
         {:else}
-            <p class="text-[256px] dark:text-white text-center">{time.value.minutes}:{time.value.seconds}.{time.value.miliseconds}</p>
+            <p class="dark:text-white text-center" style="font-size: clamp(2rem, 10vw, 16rem)">{time.value.minutes}:{time.value.seconds}.{time.value.miliseconds}</p>
         {/if}
     {:else if !timerStart && spacebarPressed}
         {#if time.value.minutes == 0}
-            <p class="text-[256px] dark:text-red-500 text-center">{time.value.seconds}.{time.value.miliseconds}</p>
+            <p class="dark:text-red-500 text-center" style="font-size: clamp(2rem, 10vw, 16rem)">{time.value.seconds}.{time.value.miliseconds}</p>
         {:else}
-            <p class="text-[256px] dark:text-red-500 text-center">{time.value.minutes}:{time.value.seconds}.{time.value.miliseconds}</p>
+            <p class="dark:text-red-500 text-center" style="font-size: clamp(2rem, 10vw, 16rem)">{time.value.minutes}:{time.value.seconds}.{time.value.miliseconds}</p>
         {/if}
     {:else}
-        <p class="text-[256px] dark:text-white text-center">Solving</p>
+        <p class="dark:text-white text-center" style="font-size: clamp(2rem, 10vw, 16rem)">Solving</p>
     {/if}
 </div>
